744813ab859708c1de32237e86cfa8f6abae28c3
Abdou <abderrahman.nemmour@univ-constantine2.dz>
2026-05-14 16:59:41 +0100

    Fix:remove old version front-end

--- Front-end/src/api/publicApi.js
+++
@@ -1,6 +0,0 @@
-import api from './axios';
-
-export const getPublicCompanies = () => api.get('/api/administration/public/companies/');
-export const getPublicOffers = () => api.get('/api/administration/public/offers/');
-export const getUniversities = () => api.get('/api/user/universities/');
-export const getAllSkills = () => api.get('/api/student/skills/');
